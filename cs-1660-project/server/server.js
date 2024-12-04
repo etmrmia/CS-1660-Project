a679cf82278be435cbfcb2634b96516857e4a74e
Alexandra Butler <butler0alex@gmail.com>
2024-12-03 19:14:46 -0500

    Updating server configuration

--- cs-1660-project/server/server.js
+++ cs-1660-project/server/server.js
@@ -1,24 +1,18 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const port = 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '../dist/cs-1660-project/browser')));
 
-app.use('/', express.static('dist/cs-1660-project'))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist/cs-1660-project/browser/index.html'));
+});
 
-// app.use(express.static(path.join(__dirname, '../dist/cs-1660-project/browser')));
-
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../src/index.html'));
-// });
-
-// app.get('/browser', function(req, res) {
-    
-// });
+app.get('/', (req, res) => {
+  res.send('Hello World from Node.js server!');
+});
 
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
+  console.log(`Server listening at http://localhost:${port}`);
 });
