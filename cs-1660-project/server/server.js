58ffc9695552306660d7905f7bd7aafa9d7dcaa3
Madeleine Lasky <mlasky2000@hotmail.com>
2024-12-11 17:25:27 -0500

    Updated queries

--- cs-1660-project/server/server.js
+++ cs-1660-project/server/server.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import express from 'express';
+import bodyParser from 'body-parser';
 const app = express();
 const port = 3000;
 import qrCode from 'qrcode';
@@ -7,6 +8,8 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../dist/cs-1660-project/browser')));
 
@@ -22,28 +25,35 @@ app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
 
-app.post("/data", function (req, res) {
-  console.log("Testing 1 2 3...");
-  // Will eventually receive data from post request or pull from database
-  // But for now here's a const to represent data to encode in QR Code
-  let data = {
-    sectionNo : 101,
-    courseID : "TEST1",
-    profID : 0
-  }
-  // If sent from client side - reference as req.body.[property name]
-  qrCode.toString(JSON.stringify(data), {type:'terminal'},
-    function(err, code) {
-      if (err) {
-        console.log(err);
-        return;
-      }
+// // DB CONNECTION:  you MUST authenticate to google cli before using this 
+// // Additionally, I will need to assign IAM roles to your google accounts so that you are able to authenticate
+// Note: may need to do npm install pg & npm install @google-cloud/cloud-sql-connector to use these
+import pg from 'pg';
+import {Connector} from '@google-cloud/cloud-sql-connector';
+const {Pool} = pg;
+const connector = new Connector();
 
-      res.setHeader('Content-Type', 'application/json');
-      res.send(JSON.stringify({qrCode : code}));
-    }
-  )
+const clientOpts = await connector.getOptions({
+  instanceConnectionName: 'cs1660-finalproject:us-central1:cs1660-finalproject',
+  ipType: 'PUBLIC',
 });
+
+const pool = new Pool({
+  ...clientOpts,
+  user: 'connect',
+  password: 'connectpassword',
+  database: 'postgres',
+  max: 5,
+});
+
+/**
+ * Both pools and connectors should be closed after done with them.
+*/
+async function closeDB(pool) {
+  await pool.end();
+  connector.close();
+}
+
 
 // Endpoint encodes qr code as Base64 string and sends this information
 app.post('/qrcode', express.json(), async (req, res) => {
@@ -64,66 +74,41 @@ app.post('/qrcode', express.json(), async (req, res) => {
 )
 });
 
-// Endpoint encodes qr code as Base64 string and sends this information
+// Endpoint to send back if a student attended 
 app.post('/getAttendance', express.json(), async (req, res) => {
  
-  console.log("Getting student attendance");
-  console.log(req.body);
+  //Testing
+  // console.log("Getting student attendance");
+  // console.log(req.body);
+  // console.log(req.body["studentID"]);
+  // console.log(req.body["courseID"]);
+  // console.log(req.body["sectionNo"]);
 
-  // Query here for student attendance and then send result if they did attend within past 24 hrs
+  // Query to see if student has already attended
+  const query = `SELECT *
+                FROM gititdonedb.ATTENDANCE 
+                WHERE studentid = ${req.body["studentID"]} AND courseid ='${req.body["courseID"]}' AND sectionno = ${req.body["sectionNo"]} AND attendancedate > CURRENT_TIMESTAMP - interval'1 day';`;
+  var rows = await pool.query(query);
+  // Testing: 
+  // console.log(rows.rowCount);
+
+  // If no rows found, then return false, student has not attended, else true 
   res.setHeader('Content-Type', 'application/json');
-  if(true)
-    res.send({attended:"True"});
-  else
+  if (rows.rowCount == 0) 
     res.send({attended:"False"});
+  else
+    res.send({attended:"True"});
 
 });
 
 app.post('/recordAttendance', express.json(), async (req, res) => {
   console.log("Recording student attendance");
   console.log(req.body);
-  // INSERT INTO DB HERE 
+  // INSERT INTO DB HERE
+  const query = `INSERT INTO gititdonedb.attendance(attendancedate, sectionno, courseid, studentid) VALUES
+                (CURRENT_TIMESTAMP, ${req.body["sectionNo"]},'${req.body["courseID"]}', ${req.body["studentID"]});`;
+   await pool.query(query);
+  // TESTING: 
+  // const {rows} = await pool.query('SELECT * FROM gititdonedb.attendance');
+  // console.table(rows);
 });
-
-
-// TODO: Create endpoint that will store DB elements after QR code has been scanned 
-// The Front End will call this API after the QR code has been scanned, and pass 
-// to it the sectionNo, courseID, studentID and timestamp of when QR was scanned 
-// This information will then be stored in the DB to officially mark the student present 
-// Endpoint to insert data
-// Below is WIP 
-// app.post('/recordAttendance', async (req, res) => {
-//   try {
-//   } catch (error) {
-//     console.error(error);
-//   }
-// });
-
-// // DB CONNECTION:  you MUST authenticate to google cli before using this 
-// // Additionally, I will need to assign IAM roles to your google accounts so that you are able to authenticate
-// Note: may need to do npm install pg & npm install @google-cloud/cloud-sql-connector to use these
-import pg from 'pg';
-import {Connector} from '@google-cloud/cloud-sql-connector';
-const {Pool} = pg;
-
-const connector = new Connector();
-const clientOpts = await connector.getOptions({
-  instanceConnectionName: 'cs1660-finalproject:us-central1:cs1660-finalproject',
-  ipType: 'PUBLIC',
-});
-
-const pool = new Pool({
-  ...clientOpts,
-  user: 'connect',
-  password: 'connectpassword',
-  database: 'postgres',
-  max: 5,
-});
-
-// ALL tables will need to be prefixed by gititdonedb to query
-const {rows} = await pool.query('SELECT * FROM gititdonedb.student');
-console.table(rows); // prints all records in student table 
-
-// Both pools and connectors should be closed after done with them
-await pool.end();
-connector.close();
